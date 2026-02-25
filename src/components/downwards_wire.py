fb33a675b9f7f06801f53f0f6976761f5dea21ee
cristian-ioan-george-chira <cristianioangeorge.chira@gmail.com>
2026-02-25 23:44:48 +0200

    Implemented downwards ORing wire

---
+++ src/components/downwards_wire.py
@@ -0,0 +1,68 @@
+#!/bin/python3
+
+from blocks import *
+from block_grid import BlockGrid
+
+class DownwardsOrWire(BlockGrid):
+    def __init__(self, inputs_count: int):
+        size_x = 3
+        size_y = 3 * inputs_count + 1
+        size_z = 4
+
+        super().__init__(size_x, size_y, size_z)
+
+        for i in range(inputs_count):
+            current_built_step = i
+            from_top_idx = inputs_count - 1 - i
+            torch_y = 3 * from_top_idx + 1
+
+            # Place the block and wire next to the torch
+            self.blocks[2][torch_y - 1][0] = Block(BASE_COMBINATIONAL_BOTTOM)
+            if current_built_step != 0:
+                self.blocks[2][torch_y][0] = Wire(WIRE_SIDE_NORTH | WIRE_SIDE_SOUTH | WIRE_SIDE_WEST)
+            else:
+                self.blocks[2][torch_y][0] = Wire(WIRE_SIDE_NORTH | WIRE_SIDE_SOUTH)
+
+            # If at the bottom, place an output pin and stop
+            if from_top_idx == 0:
+                # Place 3 blocks in a line
+                self.blocks[2][torch_y - 1][1] = Block(BASE_COMBINATIONAL_BOTTOM)
+                self.blocks[2][torch_y - 1][2] = Block(BASE_COMBINATIONAL_BOTTOM)
+                self.blocks[2][torch_y - 1][3] = Block(BASE_COMBINATIONAL_BOTTOM)
+                
+                # Place two straight wires
+                self.blocks[2][torch_y][1] = Wire(WIRE_SIDE_NORTH | WIRE_SIDE_SOUTH)
+                self.blocks[2][torch_y][2] = Wire(WIRE_SIDE_NORTH | WIRE_SIDE_SOUTH)
+
+                # Place the output repeater
+                self.blocks[2][torch_y][3] = Repeater(Directions.NORTH)
+                break
+
+            # Place the block one level down
+            self.blocks[2][torch_y - 2][1] = Block(BASE_COMBINATIONAL_BOTTOM)
+            self.blocks[2][torch_y - 1][1] = Wire(WIRE_UP_NORTH | WIRE_SIDE_WEST)
+
+            # Place the staircase blocks to the side
+            self.blocks[1][torch_y - 3][1] = Block(BASE_COMBINATIONAL_BOTTOM)
+            self.blocks[1][torch_y - 4][0] = Block(BASE_COMBINATIONAL_BOTTOM)
+
+            # Put repeater at every 3rd step (except the first and last)
+            is_not_first = current_built_step != 0
+            is_not_last = from_top_idx != 1
+            is_3rd_step = (current_built_step + 1) % 3 == 0
+
+            if is_not_first and is_not_last and is_3rd_step:
+                # Extend out the stair
+                self.blocks[0][torch_y - 3][1] = Block(BASE_COMBINATIONAL_BOTTOM)
+                self.blocks[0][torch_y - 4][0] = Block(BASE_COMBINATIONAL_BOTTOM)
+
+                # Add the wires
+                self.blocks[1][torch_y - 2][1] = Wire(WIRE_UP_EAST | WIRE_SIDE_WEST)
+                self.blocks[0][torch_y - 2][1] = Wire(WIRE_SIDE_NORTH | WIRE_SIDE_EAST)
+                self.blocks[0][torch_y - 3][0] = Wire(WIRE_UP_SOUTH | WIRE_SIDE_EAST)
+                self.blocks[1][torch_y - 3][0] = Repeater(Directions.WEST)
+
+            # Otherwise just put wires
+            else:
+                self.blocks[1][torch_y - 2][1] = Wire(WIRE_SIDE_NORTH | WIRE_UP_EAST)
+                self.blocks[1][torch_y - 3][0] = Wire(WIRE_UP_SOUTH | WIRE_SIDE_EAST)
---
+++ src/test_downwards_wire.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+from blocks import *
+from block_grid import BlockGrid
+import exporters.litematica
+import components.downwards_wire
+
+# Create Upwards wire
+downwards_wire = components.downwards_wire.DownwardsOrWire(16)
+
+# Export to litematic
+exporters.litematica.create_file(downwards_wire, "downwards_wire", "../output/downwards_wire.litematic")
+print("Saved succesfully")
